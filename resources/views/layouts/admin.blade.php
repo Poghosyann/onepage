faa6d3695b499785801124ef9cc7b32244e159f3
Poghosyann <npoghosyan96@gmail.com>
2018-07-03 15:08:18 +0400

    admin styles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blandi - Minimalist Onepage Agency HTML Template </title>
+
 <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
@@ -5,4 +14,11 @@
 <script src="{{ asset('assets/js/ckeditor/ckeditor.js') }}"></script>
 <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/js/bootstrap-filestyle.min.js') }}"></script>
+</head>
+
+<body>
+<!--  -->
+</body>
+
+</html>
 
